bed41db23d1727b9d95a607dc613747ea69f0ce4
AilinMari <ailin_scilla@hotmail.com>
2025-04-25 12:03:34 +0200

    some styling, some path changes

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -2,31 +2,42 @@
 // In order to add additional pages, you will need to implement them below
 // You may change the behaviour or approach of this file if you choose
 export default async function router(pathname = window.location.pathname) {
+  // Remove /index.html if it exists
+  if (pathname.endsWith('/index.html')) {
+    pathname = pathname.slice(0, -11); // Remove "/index.html"
+    // Ensure the path ends with a slash if it's not the root path
+    if (pathname === '') {
+      pathname = '/';
+    } else if (!pathname.endsWith('/')) {
+      pathname += '/';
+    }
+  }
+
   switch (pathname) {
     case '/':
       await import('./views/home.js');
       break;
-    // case "/auth/":
-    //   await import("./views/auth.js");
-    //   break;
-    // case "/auth/login/":
-    //   await import("./views/login.js");
-    //   break;
-    // case "/auth/register/":
-    //   await import("./views/register.js");
-    //   break;
-    // case "/post/":
-    //   await import("./views/post.js");
-    //   break;
-    // case "/post/edit/":
-    //   await import("./views/postEdit.js");
-    //   break;
-    // case "/post/create/":
-    //   await import("./views/postCreate.js");
-    //   break;
-    // case "/profile/":
-    //   await import("./views/profile.js");
-    //   break;
+    case '/auth/':
+      await import('./views/auth.js');
+      break;
+    case '/auth/login/':
+      await import('./views/login.js');
+      break;
+    case '/auth/register/':
+      await import('./views/register.js');
+      break;
+    case '/post/':
+      await import('./views/post.js');
+      break;
+    case '/post/edit/':
+      await import('./views/postEdit.js');
+      break;
+    case '/post/create/':
+      await import('./views/postCreate.js');
+      break;
+    case '/profile/':
+      await import('./views/profile.js');
+      break;
     default:
       await import('./views/notFound.js');
   }
