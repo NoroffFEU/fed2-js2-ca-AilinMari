051e200e0207cbc2a85d42fa347e7137afe1ebfe
AilinMari <ailin_scilla@hotmail.com>
2025-04-29 19:20:54 +0200

    mistake was made, repo url  update

--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -3,6 +3,6 @@ const repoUrl = "/fed2-js2-ca-AilinMari"; // Get the base URL of the repository
 export function authGuard() {
   if (!localStorage.token) {
     alert("You must be logged in to view this page");
-    window.location.href = repoUrl + "./auth/login/";
+    window.location.href = repoUrl + "/auth/login/";
   }
 }
